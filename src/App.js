992d7016f12e9ead75abbe06a775eff44f0faee8
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-06-03 21:12:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from 'react';
+const DEFAULT_NUM = 10;
 function App() {
+  const [num, setNum] = useState(DEFAULT_NUM);
+  const [table, setTable] = useState(null);
+
+  useEffect(() => {
+    const tmpTable = printMultiplicationTable();
+    setTable(tmpTable);
+  }, []);
+
+  const handleChangeNum = (e) => {
+    setNum(e.target.value);
+  };
+
+  const printMultiplicationTable = () => {
+    let table = [];
+    let row = [];
+    let counter = 0;
+    // counter starting at 1 to avoid zero padding on table
+    for (let j = 1; j <= num; j++) {
+      row = new Array(num).fill(1).map((n, i) => {
+        // i+1 to start at 1 and not at zero
+        counter += 1;
+        return (
+          <td
+            style={{ textAlign: 'center', backgroundColor: i + 1 === j ? 'yellow' : 'lightblue' }}
+            key={counter}
+          >
+            {i + 1 === j ? (
+              <b title={`${i + 1}*${j}`}>{(i + 1) * j}</b>
+            ) : (
+              <span title={`${i + 1}*${j}`}>{(i + 1) * j}</span>
+            )}
+          </td>
+        );
+      });
+      table.push(<tr key={j}>{row}</tr>);
+    }
+    return <table style={{ width: '100%' }}>{table}</table>;
+  };
+
+  const handleUpdateTable = () => {
+    const tmpTable = printMultiplicationTable(num);
+    setTable(tmpTable);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Multiplication table: <mark> {num} </mark>
+      </h1>
+      <input value={num} onChange={handleChangeNum}></input>
+      <button onClick={handleUpdateTable}>Update</button>
+      <hr />
+      {table}
     </div>
   );
 }
